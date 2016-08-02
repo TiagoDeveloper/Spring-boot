cce0005df1ba80a9af7d8f3c4e1a262a0d2f7dfc
Tiago Fonseca <tiagopereirafonseca@gmail.com>
2016-08-01 22:26:30 -0300

    adaptando ainda, usando o thymeleaf os arquivos estaticos ficam em src/main/resources e passo a usar .html ao invés de .jsp

--- src/main/java/br/com/tiagoDeveloper/Controllers/MainController.java
+++ src/main/java/br/com/tiagoDeveloper/Controllers/MainController.java
@@ -2,21 +2,23 @@ package br.com.tiagoDeveloper.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/main")
 public class MainController {
 
-	@RequestMapping("/")
+	@RequestMapping(method= RequestMethod.GET)
 	@ResponseBody
 	public String index(){
 	    return "Proudly handcrafted by " +
 	            "<a href='http://netgloo.com/en'>Netgloo</a> :)";
 	}
 	
-	@RequestMapping("/teste")
+	@RequestMapping(value="teste", method= RequestMethod.GET)
 	public String home(){
-	    return "teste";
+	    return "demo/teste";
 	}
 	
 }
